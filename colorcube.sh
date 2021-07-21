a12cf3c010ef6f162a94ade72c0ad4bc1f657e95
Pavel Lutskov <pavel.lutskov@gmail.com>
2021-07-21 21:26:50 +0200

    Update colorcube.sh with shellcheck recommends

--- colorcube.sh
+++ colorcube.sh
@@ -2,7 +2,7 @@ for i in {0..1}
 do
     for k in {0..7}
     do
-        tput setab $(($i * 8 + $k))
+        tput setab $((i * 8 + k))
         echo -n '   '
     done
     tput sgr0
@@ -13,7 +13,7 @@ for i in {0..35}
 do
     for k in {0..5}
     do
-        tput setab $((16 + $i * 6 + $k))
+        tput setab $((16 + i * 6 + k))
         echo -n '   '
     done
     tput sgr0
@@ -22,7 +22,7 @@ done
 
 for i in {232..255}
 do
-    tput setab $i
+    tput setab "$i"
     echo -n '   '
 done
 
